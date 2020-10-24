326b118e97b528c8a3fd93cd48c5149dc8f08dd1
ochaib <amassosaa@gmail.com>
2020-10-24 17:29:31 +0100

    Programming using numpy.

--- decisionTreeLearning.py
+++ decisionTreeLearning.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class TreeNode:
     treeNode = {}
 
@@ -16,31 +17,62 @@ class TreeNode:
         self.left = child
 
     def add_rightChild(self, child):
-        self.right = right
+        self.right = child
 
+    @property
     def isLeaf(self):
-        return (self.left is None) && (self.right is None)
+        return (self.left is None) & (self.right is None)
 
 
 def decision_tree_learning(training_dataset, depth):
-    if sameLabels(training_dataset) then:
+    # Load training dataset file as numpy array.
+    np_dataset = np.loadtxt(training_dataset)
+
+    if len(np.unique(np_dataset[:, 6], axis=0)) == 1:
         # Attribute refers to the index or a column of the matrix, training_dataset.
         # Create a new leaf TreeNode with the label (which is they same for all
         # entries in the dataset) as the value.
         # But looking at the dataset just because the label is the same doesn't
         # mean that the values are the same so which value are we choosing?
-        return (TreeNode(training_dataset[0][7]), depth)
+        return TreeNode(np_dataset[0][7]), depth
     else:
-        split = findSplit(training_dataset)
+        split = findSplit(np_dataset)
         (l_dataset, r_dataset) = splitOn(split)
         node = TreeNode(split)
-        (l_branch, l_depth) = decision_tree_learning(l_dataset, depth+1)
-        (r_branch, r_depth) = decision_tree_learning(r_dataset, depth+1)
-        return (node, max(l_depth, r_depth))
+        (l_branch, l_depth) = decision_tree_learning(l_dataset, depth + 1)
+        (r_branch, r_depth) = decision_tree_learning(r_dataset, depth + 1)
+        return node, max(l_depth, r_depth)
 
-def sameLabels(training_dataset):
+
+# Not necessary thanks to numpy
+def sameLabels(dataset):
     # Labels are located at the 7th index of the datasets.
-    tdBools = [training_dataset[i][7] == training_dataset[i-1][7] for i in range(len(training_dataset))]
+    tdBools = [dataset[i][7] == dataset[i - 1][7] for i in range(len(dataset))]
     return all(tdBools)
 
-def findSplit(training_dataset):
+
+def findSplit(dataset):
+    pass
+
+
+def splitOn(split):
+    pass
+
+
+def evaluateInformationGain(np_dataset, l_dataset, r_dataset):
+    return functionH(np_dataset) - remainder(l_dataset, r_dataset)
+
+
+def functionH(np_dataset):
+    # Sum of pk where pk is the number of samples with label k divided
+    # by total number of samples from initial dataset, for each label
+    # from 1 to k multiplied by the log2 of pk. Negated.
+
+    # Extract label column from dataset.
+    labels = np_dataset[:, 6]
+
+    np.log2()
+
+
+def remainder(l_dataset, r_dataset):
+    pass
