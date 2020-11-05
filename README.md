3603b6998a2f2b38855adce096b835224fca78d0
ochaib <amassosaa@gmail.com>
2020-11-05 21:54:03 +0000

    Generating accuracies, confusion matrix and other evaluation metrics for pruned decision tree.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # decision-tree-learning
 Introduction to ML - Decision Tree Coursework
+
+Instructions on how to run the decision tree code:
+
+python3 main.py "directoryname/dataset.txt"
+
+For example:
+
+python3 main.py "wifidb/noisy_dataset.txt"
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def generate_test_training(dataset, k):
     # Divide the dataset into k equal folds/splits.
     folds = np.array_split(dataset, k)
 
-    # Use k-1 (9) folds for training+validation and 1 for testing
+    # Use k-1 folds for training+validation and 1 for testing
     training_sets = []
     test_sets = []
     for i in range(k):
@@ -27,8 +27,12 @@ def main(dataset):
     np_dataset = np.loadtxt(dataset)
     k = 10
     accuracies = []
+    pruned_accuracies = []
     training_sets, test_sets = generate_test_training(np_dataset, k)
     agg_confusion_matrix = np.zeros((4, 4))
+    agg_pruned_confusion_matrix = np.zeros((4, 4))
+
+    # Evaluation on unpruned tree.
     for i in range(k):
         training_db = training_sets[i]
         test_db = test_sets[i]
@@ -38,12 +42,38 @@ def main(dataset):
         (accuracy, confusion_matrix) = evaluate(test_db, trained_tree)
         agg_confusion_matrix += confusion_matrix
         accuracies.append(accuracy)
-    #? Calculate average accuracy?
-    #? Or just select one with highest accuracy.
+    #? Calculate average accuracy
     agg_confusion_matrix /= k
     calculate_measures(agg_confusion_matrix)
     average_accuracy = np.average(accuracies)
+
+    # Tree pruning
+    inner_training_sets, validation_sets = generate_test_training(training_sets, k - 1)
+    # Evaluation on pruned tree
+    for i in range(k):
+        test_db = test_sets[i]
+        for j in range(k-1):
+            training_db = inner_training_sets[j, i]
+            validation_db = validation_sets[j, i]
+            # train
+            trained_tree, depth = train(training_db, 1)
+            # evaluation
+            (accuracy, confusion_matrix) = evaluate(test_db, trained_tree)
+            # prune
+            # TODO: Pruning function,
+            #  prune_function(trained_tree, validation_db, accuracy)
+            # evaluate on now pruned tree
+            (pruned_accuracy, pruned_confusion_matrix) = evaluate(test_db, trained_tree)
+            pruned_accuracies.append(pruned_accuracy)
+            agg_pruned_confusion_matrix += pruned_confusion_matrix
+    agg_pruned_confusion_matrix /= (k * k - 1)
+    calculate_measures(agg_pruned_confusion_matrix)
+    avg_pruned_accuracy = np.average(pruned_accuracies)
+
     print("Average Accuracy: ", average_accuracy)
+    print("Average Accuracy of Pruned Decision Tree: ", avg_pruned_accuracy)
+
+
 
 if __name__ == "__main__":
     main(sys.argv[-1])
