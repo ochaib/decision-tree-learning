863011b82b5dcee59adf2bf1e8d409f0f283b45b
Priansh Shah <shah@priansh.com>
2020-11-06 17:34:25 +0000

    revert back to PLT with agg backend

--- visualizer.py
+++ visualizer.py
@@ -1,7 +1,6 @@
 from constants import *
-import matplotlib
-matplotlib.use('Agg') # to work headless
-plt = matplotlib.pyplot # importing directly causes GDK error on Lab machines
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 
 def plot_node(node, depth=1, xmin=0, xmax=1, arrow_heads=False):
     x = (xmin + xmax) / 2
