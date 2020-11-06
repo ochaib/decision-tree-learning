Fix incorrect divisor in average confusion matrix calculation

main.py
@@ -76,12 +76,12 @@ def main(dataset):
             # evaluation
             (accuracy, confusion_matrix) = evaluate(validation_db, trained_tree)
             # prune
-            pruned_tree, depth = prune_tree(trained_tree, validation_db, accuracy)
+            pruned_tree, pruned_depth = prune_tree(trained_tree, validation_db, accuracy)
             # evaluate on now pruned tree
             (pruned_accuracy, pruned_confusion_matrix) = evaluate(test_db, pruned_tree)
             pruned_accuracies.append(pruned_accuracy)
             agg_pruned_confusion_matrix += pruned_confusion_matrix
-    agg_pruned_confusion_matrix /= (k * k - 1)
+    agg_pruned_confusion_matrix /= (k * (k - 1))
     print(agg_pruned_confusion_matrix)
     calculate_measures(agg_pruned_confusion_matrix)
     avg_pruned_accuracy = np.average(pruned_accuracies)